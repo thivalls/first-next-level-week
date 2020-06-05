3e467b0c59c6edfbb0124f383df99c8d3f91bac2
valls <olavalls@gmail.com>
2020-06-04 23:50:36 -0300

    create migrations, seeds and inserting points

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,49 @@
+import knex from './database/connection';
 import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ msg: 'ok' })
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+  const serializedItems = items.map( item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    }
+  })
+  return response.json(serializedItems);
+});
+
+routes.post('/points', async (request, response) => {
+  const {
+    image,
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = request.body;
+
+  const point = await knex('points').insert({
+    image,
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+  
+  return response.json({ success: true });
+});
+
+routes.get('/points', async (request, response) => {
+  const points = await knex('points').select('*');
+  return response.json(points);
 });
 
 export default routes;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'path';
 import express from 'express';
 import routes from './routes';
 
@@ -6,6 +7,8 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333, () => {
   console.log('running')
